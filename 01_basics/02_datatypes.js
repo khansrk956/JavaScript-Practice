9b0cb666bf2371f293f31e641cd104d12d93d772
Sharukh khan <khansrk956@gmail.com>
2024-03-08 19:40:12 +0530

    datatypes

---
+++ 01_basics/02_datatypes.js
@@ -0,0 +1,42 @@
+"use strict " ;  // treat all JS code as never version.
+
+// alert(3+3) // we are using nodejs not browser
+
+console.log( 3
+    +3) // code readability should be high
+
+console.log("Sharukh khan")
+
+let name ="Sharukh khan"
+let age = 18
+let isLoggedIn = false
+let state;
+
+// These are premitive data types.
+
+// number => 2 to power 53. 
+// bigint =>
+// string => ""
+// boolean => true, false
+//  null => This is a stand-alone value.(type)
+// undefined => declare variable but not defined value
+// symbol => unique 
+
+// Non-premitive data types.
+// object 
+const user = {
+    name:'Hiba saifi',
+    age:2,
+}
+
+const arr = [1,2,3,4,5];
+
+console.log(typeof null); // output:object --- bcz null - type is object.
+console.log(typeof undefined); // output: undefined.
+console.log(typeof "srk"); //  output: string
+console.log(typeof 14); // output: number
+console.log(typeof user); // ouput : object
+console.log(typeof arr); // output: object --- bcz array - type is object.
+console.log(typeof true); // output: boolean -- true or false are boolean.
+
+
