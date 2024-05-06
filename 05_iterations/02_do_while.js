cd0282d83420b0d79b2e9d87b71047bb73ab9b4b
Sharukh khan <khansrk956@gmail.com>
2024-05-06 15:49:42 +0530

    do-while-loop

---
+++ 05_iterations/02_do_while.js
@@ -0,0 +1,25 @@
+// while loop
+//  do-while loop
+
+let i = 0;
+while (i <= 10) {
+  console.log(`Value of Index is ${i}`);
+
+  i = i + 2;
+}
+
+const myarr = ["Apple", "Banana", "Orange", "Grapes"];
+let index = 0;
+while (index < myarr.length) {
+  console.log(`Valu is ${myarr[index]}`);
+
+  index++;
+}
+
+// do-while loop
+
+let score = 11;
+do {
+  console.log(`Score is: ${score}`);
+  score++;
+} while (score <= 10);
